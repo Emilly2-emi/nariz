4ff870bc60cd8409448a7e7e75ecdc9fe6a60fad
Emilly2-emi <100239936+Emilly2-emi@users.noreply.github.com>
2022-12-06 17:43:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ function gotPoses(results){
 
         noseY = results[0].pose.nose.y;
 
-        console.log("noseX = " + nose + "noseY = " + noseY);
-        
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
         leftWristX = results[0].pose.leftWrist.x;
 
         rightWristX = results[0].pose.rightWrist.x;
